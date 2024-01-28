b6fc793a08b28adc310c9d5918cf00dd717da52a
nictheboy <nictheboy@outlook.com>
2024-01-28 18:00:19 +0800

    test(auth): add some tests

--- src/auth/auth.spec.ts
+++ src/auth/auth.spec.ts
@@ -10,7 +10,12 @@ import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../app.module';
 import {
+  AuthenticationRequiredError,
+  PermissionDeniedError,
+} from './auth.error';
+import {
   AuthService,
+  Authorization,
   AuthorizedAction,
   authorizedActionToString,
 } from './auth.service';
@@ -56,6 +61,174 @@ describe('AuthService', () => {
     expect(() => authService.verify(token)).toThrow(
       'The token is valid, but the payload of the token is' +
         ' not a TokenPayload object. This is ether a bug or a malicious attack.',
+    );
+  });
+  it('should throw Error("The token is valid, but...")', () => {
+    const token = jwtService.sign({
+      authorization: { userId: 1, permissions: [] },
+      //signedAt: Date.now(),
+      validUntil: Date.now() + 1000,
+    });
+    expect(() => authService.verify(token)).toThrow(
+      'The token is valid, but the payload of the token is' +
+        ' not a TokenPayload object. This is ether a bug or a malicious attack.',
+    );
+  });
+  it('should throw Error("The token is valid, but...")', () => {
+    const token = jwtService.sign({
+      authorization: { userId: 1, permissions: [] },
+      signedAt: Date.now(),
+      //validUntil: Date.now() + 1000,
+    });
+    expect(() => authService.verify(token)).toThrow(
+      'The token is valid, but the payload of the token is' +
+        ' not a TokenPayload object. This is ether a bug or a malicious attack.',
+    );
+  });
+  it('should throw Error("The token is valid, but...")', () => {
+    const token = jwtService.sign({
+      authorization: {
+        userId: 1,
+        permissions: [
+          {
+            authorizedActions: [AuthorizedAction.create],
+            authorizedResource: {
+              resourceType: 'user',
+              resourceId: 1,
+            },
+          },
+        ],
+      },
+      signedAt: Date.now(),
+      validUntil: Date.now() + 1000,
+    });
+    expect(() => authService.verify(token)).toThrow(
+      'The token is valid, but the payload of the token is' +
+        ' not a TokenPayload object. This is ether a bug or a malicious attack.',
+    );
+  });
+  it('should throw Error("The token is valid, but...")', () => {
+    const token = jwtService.sign({
+      authorization: {
+        userId: 1,
+        permissions: [
+          {
+            authorizedActions: [AuthorizedAction.create],
+            authorizedResource: {
+              resourceType: 'user',
+              resourceId: 1,
+              ownedByUser: '1',
+            },
+          },
+        ],
+      },
+      signedAt: Date.now(),
+      validUntil: Date.now() + 1000,
+    });
+    expect(() => authService.verify(token)).toThrow(
+      'The token is valid, but the payload of the token is' +
+        ' not a TokenPayload object. This is ether a bug or a malicious attack.',
+    );
+  });
+  it('should throw Error("resourceOwnerId must be a number.")', () => {
+    const token = authService.sign({
+      userId: 0,
+      permissions: [],
+    });
+    expect(() => {
+      authService.audit(
+        token,
+        AuthorizedAction.other,
+        '1' as any as number,
+        'type',
+        1,
+      );
+    }).toThrow('resourceOwnerId must be a number.');
+  });
+  it('should throw Error("resourceId must be a number.")', () => {
+    const token = authService.sign({
+      userId: 0,
+      permissions: [],
+    });
+    expect(() => {
+      authService.audit(
+        token,
+        AuthorizedAction.other,
+        1,
+        'type',
+        '1' as any as number,
+      );
+    }).toThrow('resourceId must be a number.');
+  });
+  it('should throw AuthenticationRequiredError()', () => {
+    expect(() =>
+      authService.audit('', AuthorizedAction.other, 1, 'type', 1),
+    ).toThrow(new AuthenticationRequiredError());
+    expect(() =>
+      authService.audit(null, AuthorizedAction.other, 1, 'type', 1),
+    ).toThrow(new AuthenticationRequiredError());
+    expect(() =>
+      authService.audit(undefined, AuthorizedAction.other, 1, 'type', 1),
+    ).toThrow(new AuthenticationRequiredError());
+    expect(() => authService.decode('')).toThrow(
+      new AuthenticationRequiredError(),
+    );
+    expect(() => authService.decode(null)).toThrow(
+      new AuthenticationRequiredError(),
+    );
+    expect(() => authService.decode(undefined)).toThrow(
+      new AuthenticationRequiredError(),
+    );
+  });
+  it('should pass audit', () => {
+    const token = authService.sign({
+      userId: 0,
+      permissions: [
+        {
+          authorizedActions: [AuthorizedAction.query],
+          authorizedResource: {
+            ownedByUser: 1,
+            types: null,
+            resourceIds: null,
+          },
+        },
+      ],
+    });
+    authService.audit(`bearer ${token}`, AuthorizedAction.query, 1, 'type', 1);
+  });
+  it('should throw PermissionDeniedError()', () => {
+    const token = authService.sign({
+      userId: 0,
+      permissions: [
+        {
+          authorizedActions: [AuthorizedAction.delete],
+          authorizedResource: {
+            ownedByUser: null,
+            types: null,
+            resourceIds: [1, 2, 3],
+          },
+        },
+      ],
+    });
+    expect(() =>
+      authService.audit(token, AuthorizedAction.delete, 1, 'type', 5),
+    ).toThrow(new PermissionDeniedError(AuthorizedAction.delete, 1, 'type', 5));
+  });
+  it('should verify and decode successfully', () => {
+    const authorization: Authorization = {
+      userId: 0,
+      permissions: [],
+    };
+    const token = authService.sign(authorization);
+    expect(authService.verify(token)).toEqual(authorization);
+    expect(authService.verify(`Bearer ${token}`)).toEqual(authorization);
+    expect(authService.verify(`bearer ${token}`)).toEqual(authorization);
+    expect(authService.decode(token).authorization).toEqual(authorization);
+    expect(authService.decode(`Bearer ${token}`).authorization).toEqual(
+      authorization,
+    );
+    expect(authService.decode(`bearer ${token}`).authorization).toEqual(
+      authorization,
     );
   });
 });
