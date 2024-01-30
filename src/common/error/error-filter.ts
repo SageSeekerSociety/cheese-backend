886a85809b4f43c4b2f2d7478469c6dfaa2ac261
nictheboy <nictheboy@outlook.com>
2024-01-30 12:12:16 +0800

    refactor(common): remove unreachable code

--- src/common/error/error-filter.ts
+++ src/common/error/error-filter.ts
@@ -38,7 +38,7 @@ export class BaseErrorExceptionFilter implements ExceptionFilter {
     const context = host.switchToHttp();
     const response = context.getResponse<Response>();
     if (exception instanceof BaseError) {
-      const status = exception.statusCode || 500;
+      const status = exception.statusCode;
       response.status(status).json({
         code: status,
         message: `${exception.name}: ${exception.message}`,
@@ -48,16 +48,16 @@ export class BaseErrorExceptionFilter implements ExceptionFilter {
       // Above is a hint for istanbul to ignore the else branch
       // where error is logged and 'Internal Server Error' is returned.
       if (exception instanceof BadRequestException) {
-      response.status(400).json({
-        code: 400,
-        message: `${exception.name}: ${exception.message}`,
-      });
-    } else {
-      Logger.error(exception.stack);
-      response.status(500).json({
-        code: 500,
-        message: 'Internal Server Error',
-      });
+        response.status(400).json({
+          code: 400,
+          message: `${exception.name}: ${exception.message}`,
+        });
+      } else {
+        Logger.error(exception.stack);
+        response.status(500).json({
+          code: 500,
+          message: 'Internal Server Error',
+        });
       }
     }
   }
