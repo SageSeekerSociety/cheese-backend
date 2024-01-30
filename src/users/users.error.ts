7a7567fbc853ae5b058c273e8e007786d89d4ed6
nictheboy <nictheboy@outlook.com>
2024-01-30 15:57:29 +0800

    refactor(users): remove unused errors

--- src/users/users.error.ts
+++ src/users/users.error.ts
@@ -144,12 +144,6 @@ export class UserNotFollowedYetError extends BaseError {
   }
 }
 
-export class BadRequestError extends BaseError {
-  constructor(public readonly message: string) {
-    super('BadRequestError', message, 400);
-  }
-}
-
 export class FollowYourselfError extends BaseError {
   constructor() {
     super('FollowYourselfError', 'Cannot follow yourself.', 422);
@@ -163,11 +157,5 @@ export class UserAlreadyFollowedError extends BaseError {
       `User with id ${followeeId} already followed.`,
       422,
     );
-  }
-}
-
-export class UserNoProfileError extends BaseError {
-  constructor(public readonly userId: number) {
-    super('UserNoProfileError', `User with id ${userId} has no profile.`, 422);
   }
 }
