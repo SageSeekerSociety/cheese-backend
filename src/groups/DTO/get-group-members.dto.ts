fa6e814c708ba8684af90bb6ce9cd321a4ba9aab
Andy Lee <andylizf@outlook.com>
2024-01-31 12:56:25 +0800

    fix(groups): getGroupMembers page

--- src/groups/DTO/get-group-members.dto.ts
+++ src/groups/DTO/get-group-members.dto.ts
@@ -2,11 +2,9 @@ import { BaseRespondDto } from '../../common/DTO/base-respond.dto';
 import { PageRespondDto } from '../../common/DTO/page-respond.dto';
 import { UserDto } from '../../users/DTO/user.dto';
 
-export class GetGroupMembersResultDto {
-  members: UserDto[];
-  page: PageRespondDto;
-}
-
 export class GetGroupMembersRespondDto extends BaseRespondDto {
-  data: GetGroupMembersResultDto;
+  data: {
+    members: UserDto[];
+    page?: PageRespondDto;
+  };
 }
