adb474737761134e29506068aa3cb797c8ad8a31
hmx <3523763591@qq.com>
2024-02-08 22:51:01 +0800

    some error

--- src/answer/answer.error.ts
+++ src/answer/answer.error.ts
@@ -19,35 +19,7 @@ export class AnswerAlreadyAgreeError extends BaseError {
     );
   }
 }
-export class AnswerAlreadyFavoriteError extends BaseError {
-  constructor(id: number) {
-    super(
-      'AnswerAlreadyFavoriteError',
-      `Answer with id ${id} is already favorited.`,
-      400,
-    );
-  }
-}
 
-export class AnswerNotFoundError extends BaseError {
-  constructor(id: number) {
-    super(
-      'AnswerNotFoundError',
-      `Answer with id ${id} is not found.`,
-      404,
-    );
-  }
-}
-
-export class AnswerAlreadyAgreeError extends BaseError {
-  constructor(id: number) {
-    super(
-      'AnswerAlreadyAgreeError',
-      `Answer with id ${id} is already favorited.`,
-      400,
-    );
-  }
-}
 export class AnswerAlreadyFavoriteError extends BaseError {
   constructor(id: number) {
     super(
