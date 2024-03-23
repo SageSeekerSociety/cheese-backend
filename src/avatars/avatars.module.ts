7038df4cda649c2eba96f74347264d175556e8bd
nameisyui <110045515+nameisyui@users.noreply.github.com>
2024-03-23 13:35:28 +0800

    fix: change volumes

--- src/avatars/avatars.module.ts
+++ src/avatars/avatars.module.ts
@@ -12,12 +12,10 @@ import { AvatarsService } from './avatars.service';
     TypeOrmModule.forFeature([Avatar]),
     MulterModule.register({
       storage: diskStorage({
-        destination: join(
-          __dirname,
+        destination:
           process.env.NODE_ENV === 'test'
-            ? '../../test/uploads/avatars'
-            : '../../uploads/avatars',
-        ),
+            ? join(__dirname, '../../test/uploads/avatars')
+            : '/app/uploads/avatars',
         filename: (_, file, callback) => {
           const fileName = `${new Date().getTime() + extname(file.originalname)}`;
           return callback(null, fileName);
