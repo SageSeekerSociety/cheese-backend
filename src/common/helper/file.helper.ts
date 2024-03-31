1e0f8d5d427b1628c1a4c83325ae4a6d6899893a
HuanCheng65 <22636177+HuanCheng65@users.noreply.github.com>
2024-03-31 20:20:19 +0800

    fix(avatars)!: mime type & cache control

---
+++ src/common/helper/file.helper.ts
@@ -0,0 +1,18 @@
+import crypto from 'crypto';
+import * as fs from 'fs';
+
+export function getFileHash(filePath: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const hash = crypto.createHash('sha256');
+    const stream = fs.createReadStream(filePath);
+    stream.on('data', (data) => {
+      hash.update(data);
+    });
+    stream.on('end', () => {
+      resolve(hash.digest('hex'));
+    });
+    stream.on('error', (error) => {
+      reject(error);
+    });
+  });
+}
