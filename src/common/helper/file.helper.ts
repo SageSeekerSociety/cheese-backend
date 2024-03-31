a733fb6a255defc6ad019dca6290c2f3d352e48c
HuanCheng65 <22636177+HuanCheng65@users.noreply.github.com>
2024-03-31 20:28:24 +0800

    fix: close streams on end

--- src/common/helper/file.helper.ts
+++ src/common/helper/file.helper.ts
@@ -5,14 +5,17 @@ export function getFileHash(filePath: string): Promise<string> {
   return new Promise((resolve, reject) => {
     const hash = crypto.createHash('sha256');
     const stream = fs.createReadStream(filePath);
-    stream.on('data', (data) => {
-      hash.update(data);
-    });
-    stream.on('end', () => {
-      resolve(hash.digest('hex'));
-    });
-    stream.on('error', (error) => {
-      reject(error);
-    });
+    stream
+      .on('data', (data) => {
+        hash.update(data);
+      })
+      .on('end', () => {
+        resolve(hash.digest('hex'));
+        stream.close();
+      })
+      .on('error', (error) => {
+        reject(error);
+        stream.close();
+      });
   });
 }
