42052658cb07ef052de32d9fc709c5158c0151e8
nictheboy <nictheboy@outlook.com>
2024-06-17 10:51:56 +0800

    refactor: remove TypeORM completely

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,18 @@
 import { Module, ValidationPipe } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { APP_PIPE } from '@nestjs/core';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AnswerModule } from './answer/answer.module';
 import { AvatarsModule } from './avatars/avatars.module';
 import { CommentsModule } from './comments/comment.module';
-import configuration, {
-  databaseConfigFactory,
-} from './common/config/configuration';
+import configuration from './common/config/configuration';
 import { GroupsModule } from './groups/groups.module';
+import { MaterialsModule } from './materials/materials.module';
 import { QuestionsModule } from './questions/questions.module';
 import { UsersModule } from './users/users.module';
-import { MaterialsModule } from './materials/materials.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [configuration] }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: databaseConfigFactory,
-      inject: [ConfigService],
-    }),
     UsersModule,
     QuestionsModule,
     AnswerModule,
